0660aab48ca07343531f8771102ab1df60b9ed10
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-05-03 16:53:47 +0300

    updating user abilities

--- src/abilities/userAbility.ts
+++ src/abilities/userAbility.ts
@@ -1,9 +1,23 @@
 import { defineAbility } from '@casl/ability';
 
 module.exports = defineAbility((can, cannot) => {
-  can('consume', 'market');
-  can('consume', 'provider');
+
+  // Market
+  can('browse', 'market');
+  cannot('access', 'market');
+  cannot('update', 'market');
+  cannot('compute', 'market');
+  cannot('consume', 'market');
   cannot('publish', 'market');
-  cannot('delete', 'provider');
   cannot('delete', 'market');
+
+  // Provider
+  cannot('encryptUrl', 'provider');
+  cannot('initialize', 'provider');
+  cannot('access', 'provider');
+  cannot('compute', 'provider');
+
+  /// Metadatacache
+  cannot('publish', 'metadatacache');
+  cannot('update', 'metadatacache');
 });
