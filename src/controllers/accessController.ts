2fc7836750b399cba4031459c6f87e2c0f6e9637
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-05-07 14:45:23 +0300

    creating module for checking allow or deny in json files

--- src/controllers/accessController.ts
+++ src/controllers/accessController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 import test from '../authModules/test';
 import keycloak from '../authModules/keycloak';
+import json from '../authModules/json';
 
 function accessController(req: Request, res: Response){
     const { eventType, component, credentials,  authService} = req.body;
@@ -8,6 +9,9 @@ function accessController(req: Request, res: Response){
     switch(authService){
         case "test":
             test(res, credentials, eventType, component);
+            break;
+        case "json": 
+            json(res, credentials.token, eventType, component);
             break;
         case "keycloak": 
             keycloak(res, credentials.token, eventType, component);
