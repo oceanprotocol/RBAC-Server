f29295dbf6052b9cf7eced5d94adc876494da421
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-05-26 12:58:40 +0300

    looping through all users who are returned for that address

--- src/authModules/keycloakAddress.ts
+++ src/authModules/keycloakAddress.ts
@@ -5,9 +5,6 @@ interface keycloakResponse {
   id: string
   userRoles: [string]
 }
-interface resJSON {
-  keycloakResponse: [keycloakResponse]
-}
 
 export default async function address(
   res: Response,
@@ -15,25 +12,26 @@ export default async function address(
   eventType: string,
   component: string
 ): Promise<void> {
-  // Request to keyclock auth service will take place here...
   try {
     const url = `${process.env.KEYCLOAK_ADDRESS_URL}/${address}`
     const response = await fetch(url, {
       method: 'GET'
     })
-    const resJSON: resJSON = await response.json()
-    const roles = resJSON[0].userRoles
-    let result = false
-    for (let i = 0; i < roles.length; i++) {
-      result = checkAbilities(roles[i], eventType, component)
-      console.log('result', result)
-      if (result === true) {
-        break
-      } else {
-        continue
+    const resJSON: [keycloakResponse] = await response.json()
+    for (let i = 0; i < resJSON.length; i++) {
+      const roles = resJSON[i].userRoles
+      let result = false
+      for (let i = 0; i < roles.length; i++) {
+        result = checkAbilities(roles[i], eventType, component)
+        console.log('result', result)
+        if (result === true) {
+          break
+        } else {
+          continue
+        }
       }
+      res.json(result)
     }
-    res.json(result)
   } catch (error) {
     console.error(error)
     res.json(false)
