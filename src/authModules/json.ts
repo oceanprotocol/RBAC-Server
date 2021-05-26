b60b56709b42b684c39fc492079694594679a7a0
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-05-26 12:54:25 +0300

    fixing linting issues

--- src/authModules/json.ts
+++ src/authModules/json.ts
@@ -1,21 +1,21 @@
-import { Response } from 'express';
-import allow from '../data/allow.json';
-import deny from '../data/deny.json';
+import { Response } from 'express'
+import allow from '../data/allow.json'
+import deny from '../data/deny.json'
 
-export default function json(res: Response, address: string){
-    let response = "default";
-    for(let i=0; i < allow.length; i++){
-        if (allow[i] === address){
-            response = "allow"
-            break
-        }
+export default function json(res: Response, address: string): void {
+  let response = 'default'
+  for (let i = 0; i < allow.length; i++) {
+    if (allow[i] === address) {
+      response = 'allow'
+      break
     }
-    for(let i=0; i < deny.length; i++){
-        if (deny[i] === address){
-            response = "deny"
-            break
-        }
+  }
+  for (let i = 0; i < deny.length; i++) {
+    if (deny[i] === address) {
+      response = 'deny'
+      break
     }
-    console.log("Permission response:", response);
-    res.json(response);
+  }
+  console.log('Permission response:', response)
+  res.json(response)
 }
