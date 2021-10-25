285f6a17fdb0ae614289f5be0a4cd9c43eaa8e46
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-10-25 17:20:44 +0100

    Directly returning true from checkCredentials function if either allow OR deny list does not exist

--- src/authModules/authenticateProfile.ts
+++ src/authModules/authenticateProfile.ts
@@ -7,19 +7,23 @@ async function checkCredentials(
   profile: profile,
   credentials: requestCredentials
 ): Promise<boolean> {
-  for (let i = 0; i < credentialArray.length; i++) {
-    const type = credentialArray[i].type
-    for (let j = 0; j < credentialArray[i].values.length; j++) {
-      const value = credentialArray[i].values[j]
+  if (credentialArray === undefined || credentialArray.length === 0) {
+    return true
+  } else {
+    for (let i = 0; i < credentialArray.length; i++) {
+      const type = credentialArray[i].type
+      for (let j = 0; j < credentialArray[i].values.length; j++) {
+        const value = credentialArray[i].values[j]
 
-      if (type === 'address' && value === credentials.value) {
-        return true
-      } else if (value === profile[`${type}`]) {
-        return true
+        if (type === 'address' && value === credentials.value) {
+          return true
+        } else if (value === profile[`${type}`]) {
+          return true
+        }
       }
     }
+    return false
   }
-  return false
 }
 
 async function authenticateProfile(
@@ -28,22 +32,17 @@ async function authenticateProfile(
   credentials: requestCredentials,
   ddoCredentials: Credentials
 ): Promise<boolean> {
-  let isAllowed
-  if (ddoCredentials.allow === undefined || ddoCredentials.allow.length === 0) {
-    // User is default allowed if no allowList exists in the DDO
-    isAllowed = true
-  } else {
-    isAllowed = await checkCredentials(
-      ddoCredentials.allow,
-      profile,
-      credentials
-    )
-  }
+  const isAllowed = await checkCredentials(
+    ddoCredentials.allow,
+    profile,
+    credentials
+  )
   const isDenied = await checkCredentials(
     ddoCredentials.deny,
     profile,
     credentials
   )
+
   if (isAllowed === false || isDenied === true) {
     return false
   } else {
