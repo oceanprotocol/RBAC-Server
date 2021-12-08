708ff4a3745e6b5bede6f67178b26f356dcd1bab
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-12-08 17:53:57 +0300

    Updating post body

--- src/authModules/other/apiProviderAccess.ts
+++ src/authModules/other/apiProviderAccess.ts
@@ -7,11 +7,20 @@ export default async function getProfile(
   providerAddress: string
 ): Promise<boolean> {
   try {
-    const url = `${process.env.API_PROVIDER_CHECK}/${address}`
+    const walletAddress = '0xB149C36986cf8e2B260d2Eb02AFf97058a1a5C95'
+    const providerAddress = '0xf31FC0D18A24c6Ae4225A0d4eEB709AC0a18E993'
+    const data = { walletAddress, providerAddress }
+    const url = `${process.env.API_PROVIDER_CHECK}`
+    console.log('Requesting from URL:', url)
     const response = await fetch(url, {
-      method: 'POST'
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
     })
     const resJSON = await response.json()
+    console.log('resJSON', resJSON)
     return resJSON
   } catch (error) {
     // Respond false if no user profile can be found
