0e8b948b74e8ad0fa7514ea0a44589c9e36f9d7c
Soon Huat <soon_huat.phan@daimler.com>
2022-03-31 18:13:56 +0800

    upgrade oceanprotocol/lib and nft owner check

--- src/utils/getAsset.ts
+++ src/utils/getAsset.ts
@@ -1,21 +1,21 @@
-import { DDO, DID, Logger } from '@oceanprotocol/lib'
+import { Asset, LoggerInstance } from '@oceanprotocol/lib'
 import axios, { AxiosResponse } from 'axios'
 
-export default async function getDDO(did: string | DID): Promise<DDO> {
+export default async function getAsset(did: string): Promise<Asset> {
   try {
     const metadataCacheUri = process.env.METADATACACHE_URI
-    const response: AxiosResponse<DDO> = await axios.get(
-      `${metadataCacheUri}/api/v1/aquarius/assets/ddo/${did}`
+    const response: AxiosResponse<Asset> = await axios.get(
+      `${metadataCacheUri}/api/aquarius/assets/ddo/${did}`
     )
     if (!response || response.status !== 200 || !response.data) return
 
-    const data = { ...response.data }
-    return new DDO(data)
+    const data = { ...response.data } as Asset
+    return data
   } catch (error) {
     if (axios.isCancel(error)) {
-      Logger.log(error.message)
+      LoggerInstance.log(error.message)
     } else {
-      Logger.error(error.message)
+      LoggerInstance.error(error.message)
     }
   }
 }
