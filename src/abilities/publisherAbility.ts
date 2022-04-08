c61cf2e2540fcb8f5b39d766f8ae96693cf285b0
Soon Huat <soon_huat.phan@daimler.com>
2022-04-08 15:44:57 +0800

    refine consumer and publisher role access

--- src/abilities/publisherAbility.ts
+++ src/abilities/publisherAbility.ts
@@ -1,21 +1,21 @@
 import { defineAbility } from '@casl/ability'
 
-export default defineAbility((can) => {
+export default defineAbility((can, cannot) => {
   // Market
   can('browse', 'market')
-  can('consume', 'market')
-  can('compute', 'market')
   can('access', 'market')
   can('update', 'market')
   can('publish', 'market')
   can('delete', 'market')
+  cannot('consume', 'market')
+  cannot('compute', 'market')
 
   // Provider
   can('access', 'provider')
-  can('consume', 'provider')
-  can('compute', 'provider')
   can('encryptUrl', 'provider')
-  can('initialize', 'provider')
+  cannot('consume', 'provider')
+  cannot('compute', 'provider')
+  cannot('initialize', 'provider')
 
   /// Metadatacache
   can('publish', 'metadatacache')
